7356261b8a769f890513b67d0d85bc6a0eda771b
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-04-18 15:54:52 +0630

    Use Recursion to Create a Range of Numbers

---
+++ js/rangeOfNumbers.js
@@ -0,0 +1,13 @@
+function rangeOfNumber(start, end) {
+    if (start > end) {
+        return "StartNum must not be greater than EndNum!";
+    } else if (start === end) {
+        return [end];
+    } else {
+        const array = rangeOfNumber(start, end - 1);
+        array.push(end);
+        return array;
+    }
+}
+
+console.log(rangeOfNumber(1, 0));
