54ae116116b5d719432cb343a68bc30f199c2cdf
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-05-05 09:13:24 +0630

    querying database

--- helloworld/app/app.js
+++ helloworld/app/app.js
@@ -1,39 +1,9 @@
-import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
-import { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
+import postgres from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
 
-configure({
-  views: `${Deno.cwd()}/views/`,
+const sql = postgres({});
+
+const rows = await sql`SELECT * FROM names`;
+
+rows.forEach((obj) => {
+  console.log(obj.name);
 });
-
-const responseDetails = {
-  headers: { "Content-Type": "text/html;charset=UTF-8" },
-};
-
-const data = {
-  names: [],
-};
-
-const redirectTo = (path) => {
-  return new Response(`Redirecting to ${path}.`, {
-    status: 303,
-    headers: {
-      "Location": path,
-    },
-  });
-};
-
-const addName = async (request) => {
-  const formData = await request.formData();
-  data.names.push(formData.get("name"));
-};
-
-const handleRequest = async (request) => {
-  if (request.method === "POST") {
-    await addName(request);
-    return redirectTo(request.url);
-  }
-
-  return new Response(await renderFile("index.eta", data), responseDetails);
-};
-
-serve(handleRequest, { port: 7777 });
--- helloworld/flyway/sql/V1__initial_database.sql
+++ helloworld/flyway/sql/V1__initial_database.sql
@@ -1,4 +1,10 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+CREATE TABLE names (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE addresses (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  address TEXT NOT NULL
 );
